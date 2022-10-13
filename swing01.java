6fbdb0b991b42f57bd8e6dcae7c4aa0e622ef7a0
perlapaola22 <114268132+perlapaola22@users.noreply.github.com>
2022-10-13 14:11:55 -0600

    Update swing01.java

--- swing01.java
+++ swing01.java
@@ -1,29 +1,20 @@
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Polygon;
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
 
 
-    
+public class Swing02 {
 
-public class Swing01 {
-
-    
     public static void main(String args[]) {
-      JFrame window = new JFrame("Swing");
-      MyCanvas canvas = new MyCanvas();
-      
-      window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-      window.setSize(420,400);
-      window.add(canvas);
-      window.pack();
-      window.setResizable(true);
-      window.setLocationRelativeTo(null);
-      window.setVisible(true);
-      
-     
-      
+        JFrame window = new JFrame("SWING");
+        MyCanvas canvas = new MyCanvas();
+        
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setSize(400,300);
+        window.add(canvas);
+        window.pack();
+        window.setResizable(false);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
         
     }
     
