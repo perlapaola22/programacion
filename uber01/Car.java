c838193a2be646fc7c78e413fad88527dbe713b8
perlapaola22 <114268132+perlapaola22@users.noreply.github.com>
2022-10-28 08:43:45 -0600

    Add files via upload

---
+++ uber01/Car.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uber01;
+
+import java.awt.Color;
+
+public class Car extends Vehicle {
+    String matricula;
+    
+    
+    public Car(String matricula, Color color, String model){
+        super(matricula,color,model);
+        this.color = color;
+        this.maticula = matricula;
+        this.model = model;
+        
+    }
+    public String getModel(){
+        return model;
+    }
+    public String matricula(){
+        return matricula;
+    }
+    
+}
